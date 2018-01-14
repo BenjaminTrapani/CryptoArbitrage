c7ba184b039c9de812359383e12cc19c1e38859c
BenTrapani <ben.trapani1995@gmail.com>
2018-01-14 13:47:56 -0500

    Update algorithm comment doc

--- CryptoArbitrage/src/main/java/BenTrapani/CryptoArbitrage/OrderBookAggregator.java
+++ CryptoArbitrage/src/main/java/BenTrapani/CryptoArbitrage/OrderBookAggregator.java
@@ -85,7 +85,7 @@ public class OrderBookAggregator {
 	// Edges are built from orders in the order book. Each edge contains an
 	// action (buy/sell),
 	// dest currency, price and quantity. Ratio can be derived by quantity /
-	// price. Multiply ratios along
+	// price for buy, and price / quantity for sell. Multiply ratios along
 	// a loop. If final ratio is > 1, execute all trades along path.
 	// Edge construction:
 	// If order book entry is buy, add edge with action sell from base to
@@ -103,7 +103,8 @@ public class OrderBookAggregator {
 	// If all trades are successful, the only change in currency allocation will
 	// be an increase in the source currency by the given ratio.
 	// If one or more trades fail, the allocation among currencies will become
-	// lopsided but an increase in source currency will still be observed.
+	// lopsided but an increase in source currency will still be observed (assuming that trades
+	// connected to and from source currency all succeed)
 	public Disposable[] createConsumerForExchange(StreamingExchange exchange) {
 		String exchangeName = exchange.getExchangeSpecification().getExchangeName();
 		Set<CurrencyPair> currenciesForExchange = exchange.getExchangeMetaData().getCurrencyPairs().keySet();
